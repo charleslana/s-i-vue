bf75b18d411104aee91f80d436991a65a98df396
Charles <charleslanop@gmail.com>
2023-01-30 23:41:34 -0300

    update

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = 'Super Island';
+      args[0].title = 'Super Island - Online RPG Game';
       return args;
     });
   },
